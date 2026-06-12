48207fe57b9ef8417093f1ce33ceb8fdd157d60e
actualpug <actualpug@gmail.com>
2026-06-11 22:35:49 -0600

    big merge prep

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const base = process.env.VITE_BASE_PATH ?? (process.env.GITHUB_ACTIONS ? '/schematic-editor/' : '/');
-
 export default defineConfig({
-  base,
+  base: process.env.GITHUB_ACTIONS ? '/schematic-editor/' : '/',
   plugins: [react()],
 });
